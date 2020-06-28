7410606aac8b27182b2a686662989e18c73654b5
Jonas Funch <floefunch@live.dk>
2020-06-28 16:44:28 +0200

    added assignment

--- a2.py
+++ a2.py
@@ -1,10 +1,35 @@
 import argparse
 import random
+
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.base import is_classifier
-import numpy as np
-random.seed(42)
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.base import is_classifier
+from sklearn.decomposition import TruncatedSVD
 
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+
+
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+#for bonus
+from sklearn.decomposition import PCA
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
+
+
+random.seed(42)
 
 ###### PART 1
 #DONT CHANGE THIS FUNCTION
@@ -16,12 +41,61 @@ def part1(samples):
     print("Data shape: ", X.shape)
     return X
 
-def extract_features(samples):
-    print("Extracting features ...")
-    data = fetch_20newsgroups(subset='all')
-    print(data[0])
+#Lav tokenizer sep.
 
-print(extract_features)
+dataone = [""" was wondering if anyone out there could enlighten me on this car I saw
+the other day. It was a 2-door sports car, looked to be from the late 60s/
+early 70s. It was called a Bricklin. The doors were really small. In addition,
+the front bumper was separate from the rest of the body. This is
+all I know. If anyone can tellme a model name, engine specs, years
+of production, where this car is made, history, or whatever info you
+have on this funky looking car, please e-mail."""]
+
+data = fetch_20newsgroups(subset ='all', shuffle=True, random_state=42)
+
+    
+#add preproccessing
+
+
+def tokenisation(text):
+    # lower -> tokenization/divide on whitespace (meaning no n-grams) ->
+    # alphabeticals -> stopwords
+    lower_tokens = text.lower() 
+    tokens = lower_tokens.split(' ')
+    processed = [word for word in tokens if word.isalpha()]
+    #stop_words = [word for word in processed if word not in ENGLISH_STOP_WORDS]
+    #counts = [word for word in stop_words if stop_words.count(word) > 5]
+
+    return processed
+
+
+
+#print(tokenisation(dataone))
+
+def extract_features(samples):
+    # all articles are tokenised, put in dict and added to list
+    # the list of dicts are made vectors and transformed into arrays
+    print("Extracting features ...")
+    list_of_articles = []
+    for i in samples:
+        article = dict()
+        words = tokenisation(i)
+        for word in words:
+            if word in article:
+                article[word] += 1
+            else:
+                article[word] = 1
+        
+        list_of_articles.append(article)
+    vector = DictVectorizer()
+    array = vector.fit_transform(list_of_articles).toarray()
+    return array
+        
+        
+        
+        
+#print(extract_features(data.data))
+#print(extract_features(data.data))
 
 
 ##### PART 2
@@ -39,17 +113,19 @@ def part2(X, n_dim):
 
 def reduce_dim(X,n=10):
     #fill this in
-    pass
+    svd = PCA(n_components = n)
+    svd.fit(X)
+    return svd.transform(X)
 
-
+#print(reduce_dim(extract_features(data.data)))
 
 ##### PART 3
 #DONT CHANGE THIS FUNCTION EXCEPT WHERE INSTRUCTED
 def get_classifier(clf_id):
     if clf_id == 1:
-        clf = "" # <--- REPLACE THIS WITH A SKLEARN MODEL
+        clf = KNeighborsClassifier() # <--- REPLACE THIS WITH A SKLEARN MODEL
     elif clf_id == 2:
-        clf = "" # <--- REPLACE THIS WITH A SKLEARN MODEL
+        clf = GaussianNB() # <--- REPLACE THIS WITH A SKLEARN MODEL
     else:
         raise KeyError("No clf with id {}".format(clf_id))
 
@@ -85,17 +161,44 @@ def part3(X, y, clf_id):
 
 
 def shuffle_split(X,y):
-    pass # Fill in this
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    return X_train, X_test, y_train, y_test
 
 
 def train_classifer(clf, X, y):
     assert is_classifier(clf)
-    ## fill in this
+    trained_model = clf.fit(X,y)
+    return trained_model
 
+   
 
 def evalute_classifier(clf, X, y):
     assert is_classifier(clf)
     #Fill this in
+    predict = clf.predict(X)
+    accuracy = accuracy_score(y, predict, normalize=True)
+    precision = precision_score(y, predict, average = "macro")
+    recall = recall_score(y, predict, average = "macro")
+    f_measure = f1_score(y, predict, average = "macro")
+    classification = classification_report(y, predict)
+
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("recall: ", recall)
+    print("F-Measure: ", f_measure)
+    print("Classification: ", classification)
+
+    
+    
+    
+#print(part3(part2(part1(data.data), 500), data.target, 2))
+#print(part3(part2(part1(data.data), 100), data.target, 2))
+#print(part3(part2(part1(data.data), 50), data.target, 2))
+#print(part3(part2(part1(data.data), 25), data.target, 2))
+#print(part3(part2(part1(data.data), 10), data.target, 2))
+print(part3(part1(data.data), data.target, 2))  
+
+
 
 
 ######
@@ -130,7 +233,6 @@ def main(model_id=None, n_dim=False):
     if model_id:
         print("\n------------PART 3-----------")
         part3(X, labels, model_id)
-
 
 
 if __name__ == '__main__':
